4de74c8d39850cb5d4c2af2f3c4332b16fe30c69
Diogo Lara <diogo.lara.dev@gmail.com>
2024-10-02 07:56:55 -0300

    refactor: adjust execution in single command + improved seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,46 +1,49 @@
 import { PrismaClient } from '@prisma/client';
 
 async function seed() {
-  const prisma = new PrismaClient();
+	const prisma = new PrismaClient();
 
-  try {
-    await prisma.$connect();
+	try {
+		await prisma.$connect();
 
-    await prisma.$transaction(async (tx) => {
-      await tx.location.createMany({
-        data: [
-          { name: 'Glades', state: 'FL' },
-          { name: 'Gulf', state: 'FL' },
-          { name: 'Hamilton', state: 'FL' },
-          { name: 'Hardee', state: 'FL' },
-          { name: 'Hendry', state: 'FL' },
-          { name: 'El Paso', state: 'TX' },
-          { name: 'Erath', state: 'TX' },
-          { name: 'Falls', state: 'TX' },
-          { name: 'Fannin', state: 'TX' },
-          { name: 'Fayette', state: 'TX' },
-          { name: 'Fisher', state: 'TX' },
-        ],
-      });
+		const count = await prisma.location.count();
+		if (count > 0) return;
 
-      await tx.service.createMany({
-        data: [
-          { name: 'Access Control Software' },
-          { name: 'Air Conditioning' },
-          { name: 'Landscaping' },
-          { name: 'Landscaping Maintenance' },
-          { name: 'Snow and Ice Removal' },
-          { name: 'Sewer and Water Pipelining' },
-        ],
-      });
-    });
+		await prisma.$transaction(async (tx) => {
+			await tx.location.createMany({
+				data: [
+					{ name: 'Glades', state: 'FL' },
+					{ name: 'Gulf', state: 'FL' },
+					{ name: 'Hamilton', state: 'FL' },
+					{ name: 'Hardee', state: 'FL' },
+					{ name: 'Hendry', state: 'FL' },
+					{ name: 'El Paso', state: 'TX' },
+					{ name: 'Erath', state: 'TX' },
+					{ name: 'Falls', state: 'TX' },
+					{ name: 'Fannin', state: 'TX' },
+					{ name: 'Fayette', state: 'TX' },
+					{ name: 'Fisher', state: 'TX' },
+				],
+			});
 
-    console.log('Database seed succeeded');
-  } catch {
-    console.error('[seed@database.seed] Failed to seed database');
-  }
+			await tx.service.createMany({
+				data: [
+					{ name: 'Access Control Software' },
+					{ name: 'Air Conditioning' },
+					{ name: 'Landscaping' },
+					{ name: 'Landscaping Maintenance' },
+					{ name: 'Snow and Ice Removal' },
+					{ name: 'Sewer and Water Pipelining' },
+				],
+			});
+		});
 
-  await prisma.$disconnect();
+		console.log('Database seed succeeded');
+	} catch {
+		console.error('[seed@database.seed] Failed to seed database');
+	}
+
+	await prisma.$disconnect();
 }
 
 seed();
